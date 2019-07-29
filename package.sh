5b58683eee1bbfca2776a396ef94817430498db7
jnzheng <jnzheng@jnzhengs-MacBook-Pro.local>
2019-07-29 16:03:18 +0800

    remove artifacts for now

--- package.sh
+++ package.sh
@@ -1,8 +1,8 @@
 echo "------ package task ----- "
 
-touch my-artifact.html
+# touch my-artifact.html
 
-echo "hello, this is my artifact from package job" > my-artifact.html
-zip my-artifact.zip my-artifact.html
+# echo "hello, this is my artifact from package job" > my-artifact.html
+# zip my-artifact.zip my-artifact.html
 
-echo "------ package task finish ----- "
+# echo "------ package task finish ----- "
