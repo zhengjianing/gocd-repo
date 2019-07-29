7b7475cb1c7b3b19d284ba1a36eec5004f1c126a
jnzheng <jnzheng@jnzhengs-MacBook-Pro.local>
2019-07-29 14:35:08 +0800

    create artifacts

--- package.sh
+++ package.sh
@@ -1,1 +1,8 @@
-echo "------ package task ----- "
+echo "------ package task start ----- "
+
+# filename="my-artifact-${GO_PIPELINE_COUNTER}.html"
+# touch $filename
+
+# echo "hello" > $filename
+
+echo "------ package task finish ----- "
