4b2678252e3147181849c46489341c6c7fb4cc36
jnzheng <jnzheng@jnzhengs-MacBook-Pro.local>
2019-07-29 14:21:33 +0800

    modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 echo "------ deploy start ----- "
-echo "--------- deploying to $1, version: ${GO_PIPELINE_COUNTER} ...... --------"
+
+echo "--------- parameter: deploying to $1 envrionment now...... --------"
+
+echo "--------- environment variable: GO_PIPELINE_COUNTER from GoCD itself: ${GO_PIPELINE_COUNTER} ...... --------"
+echo "--------- environment variable: username from customer environment variable: ${USERNAME} ...... --------"
+
 echo "------ deploy finish ----- "
