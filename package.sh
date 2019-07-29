4ed930c62371cc199bb41dbedff7c14b8e048e0d
jnzheng <jnzheng@jnzhengs-MacBook-Pro.local>
2019-07-29 15:19:28 +0800

    zip artifact

--- package.sh
+++ package.sh
@@ -3,5 +3,6 @@ echo "------ package task ----- "
 touch my-artifact.html
 
 echo "hello, this is my artifact from package job" > my-artifact.html
+zip my-artifact.zip my-artifact.html
 
 echo "------ package task finish ----- "
