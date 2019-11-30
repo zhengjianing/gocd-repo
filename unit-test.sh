ecffa75e723637cf0d77eb7af217e0551d40af80
Zheng Jianing <jnzheng@thoughtworks.com>
2019-11-30 20:07:50 +0800

    test env

--- unit-test.sh
+++ unit-test.sh
@@ -4,5 +4,6 @@ echo "modify test again ------ unit test start ----- "
 
 # echo "==== if test failed, we should throw exception from script."
 # exit 1
+echo "--------- API_TOKEN from environment variable: ${API_TOKEN} ...... --------"
 
 echo "------ unit test finish ----- "
